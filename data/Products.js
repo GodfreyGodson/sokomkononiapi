db7d585934154a8056999f99a5b9f2e9dc94e6aa
caudle <lemaemanuel96@gmail.com>
2023-01-11 10:53:31 +0300

    soko api

---
+++ data/Products.js
@@ -0,0 +1,101 @@
+const products = [
+  {
+    name: "Tomato High Quality",
+    image: "https://images-prod.healthline.com/hlcmsresource/images/AN_images/tomatoes-1296x728-feature.jpg",
+    description:
+      "Get yours now",
+    price: 89000,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
+  {
+    name: "Maize Grade one",
+    image: "https://m.media-amazon.com/images/I/61V1TTqTu6L._SL1023_.jpg",
+    description:
+      "Buy now at lowest cost",
+    price: 5990,
+    countInStock: 10,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "Rice Super",
+    image: "https://economictimes.indiatimes.com/thumb/msid-91797214,width-1200,height-900,resizemode-4,imgsize-154184/rice.jpg?from=mdr",
+    description:
+      "Mchele Super",
+    price: 92900,
+    countInStock: 10,
+    rating: 3.5,
+    numReviews: 3,
+  },
+  {
+    name: "Beans Highly Protein",
+    image: "https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/kidney-beans-1296x728-feature.jpg?w=1155&h=1528",
+    description:
+      "Highly Recommended buy them now",
+    price: 3990,
+    countInStock: 0,
+    rating: 5,
+    numReviews: 9,
+  },
+  {
+    name: "Meat For Your Health",
+    image: "https://cdn.britannica.com/72/143572-050-87DF1262/pork-butcher-shop-Hong-Kong.jpg",
+    description:
+      "Highly Recommended for your health",
+    price: 9000,
+    countInStock: 7,
+    rating: 2,
+    numReviews: 2,
+  },
+  {
+    name: "white and Normal Sugar",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfwKqEfCDJBjSgiVr089d0hh_Dz_pst6N5mw&usqp=CAU",
+    description:
+      "Buy now in Bulk",
+    price: 4000,
+    countInStock: 50,
+    rating: 0,
+    numReviews: 0,
+  },
+
+
+
+  {
+    name: "Sunflower Oil",
+    image: "https://kumarmetal.com/wp-content/uploads/2022/04/sunflower-oil-properties-and-manufacture-1.png",
+    description:
+      "Buy now yours",
+    price: 8900,
+    countInStock: 9,
+    rating: 0,
+    numReviews: 0,
+  },
+
+
+  {
+    name: "Onions for your health",
+    image: "https://cdn.britannica.com/08/187208-050-E4233521/onions.jpg",
+    description:
+      "Buy them now at lowest cost",
+    price: 6000,
+    countInStock: 4,
+    rating: 0,
+    numReviews: 0,
+  },
+
+
+  {
+    name: "Water Melons",
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO2osJzkYvUj-j3SZAghCSQT05GS3fOtXEeQxSamTGeoUojQtcRI3vham-ZuMjXCiIogE&usqp=CAU",
+    description:
+      "Buy them now at lowest Cost",
+    price: 85000,
+    countInStock: 2,
+    rating: 0,
+    numReviews: 3,
+  },
+];
+
+export default products;
